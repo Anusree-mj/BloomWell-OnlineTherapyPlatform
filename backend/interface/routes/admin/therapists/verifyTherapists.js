28ec14b66267a64c6d821f38c6805ce06fe38310
Anusree-mj <anuanusree682@gmail.com>
2024-05-29 21:03:44 +0530

    arrange admin routes codes

--- backend/interface/routes/admin/therapists/verifyTherapists.js
+++ backend/interface/routes/admin/therapists/verifyTherapists.js
@@ -4,78 +4,16 @@ import { protectAdmin } from '../../../middlewares/adminAuthMiddleware.js';
 import controllers from '../../../../useCases/index.js'
 
 // get therapists details
-router.get('/view', protectAdmin, async (req, res) => {
-    try {
-        console.log('entered in get client details')
-        const { therapists } = await controllers.adminControllers.getTherapistsDetailsController();
-        if (therapists) {
-            console.log(therapists, 'therapists details')
-            res.status(200).json({
-                status: 'ok',
-                therapists: therapists
-            });
-        }
-    } catch (err) {
-        res.status(401).json({ status: 'nok', message: err.message })
-        console.log(err)
-    }
-})
+router.get('/view', protectAdmin, controllers.adminControllers.getTherapistsDetailsController)
 
 // verify therapist
-router.post('/:therapistId/verify', protectAdmin, async (req, res) => {
-    try {
-        console.log('entered in verify routes')
-        const therapistId = req.params.therapistId
-        const { verifyStatus } = req.body;
-        const { status } = await controllers.adminControllers.verifyTherapistController(
-            therapistId, verifyStatus
-        )
-        if (status === 'ok') {
-            res.status(200).json({
-                status: 'ok',
-                message: 'Therapist verified succesfully'
-            });
-        }
-    } catch (err) {
-        res.status(401).json({ status: 'nok', message: 'Invalid entry' })
-        console.log(err)
-    }
-})
+router.post('/:therapistId/verify', protectAdmin, controllers.adminControllers.verifyTherapistController)
+
 // delete therapist
-router.delete('/:therapistId', protectAdmin, async (req, res) => {
-    try {
-        console.log('entered in delete routes')
-        const therapistId = req.params.therapistId
-        const { status } = await controllers.adminControllers.deleteTherapistController(therapistId);
-        if (status === 'ok') {
-            res.status(200).json({
-                status: 'ok',
-                message: 'User blocked succesfully'
-            });
-        } else {
-        }
-    } catch (err) {
-        res.status(401).json({ status: 'nok', message: 'Invalid entry' })
-        console.log(err)
-    }
-})
+router.delete('/:therapistId', protectAdmin, controllers.adminControllers.deleteTherapistController)
 
 // edit therapsit
-router.put('/:therapistId', protectAdmin, async (req, res) => {
-    try {
-        console.log('entered in edit routes')
-        const therapistId = req.params.therapistId
-        const { status } = await controllers.adminControllers.editTherapistController(therapistId);
-        if (status === 'ok') {
-            res.status(200).json({
-                status: 'ok',
-                message: 'User unblocked succesfully'
-            });
-        } else {
-        }
-    } catch (err) {
-        res.status(401).json({ status: 'nok', message: 'Invalid entry' })
-        console.log(err)
-    }
-})
+router.put('/:therapistId', protectAdmin, controllers.adminControllers.editTherapistController)
+
+
 export default router;
--- backend/useCases/admin/therapistManagement/manageTherapistController.js
+++ backend/useCases/admin/therapistManagement/manageTherapistController.js
@@ -1,48 +1,70 @@
 import manageTherapistQueries from "../../../infrastructure/dbQueries/admin/manageTherapistQueries.js";
 
 // get therapists
-const getTherapistsDetailsController = async () => {
+const getTherapistsDetailsController = async (req, res) => {
     try {
         const { therapists } = await manageTherapistQueries.getTherapistsDetailsQuery()
-        return { therapists }
+        if (therapists) {
+            res.status(200).json({
+                status: 'ok',
+                therapists: therapists
+            });
+        }
     } catch (err) {
+        res.status(401).json({ status: 'nok', message: err.message })
         console.log('Error found', err)
 
     }
 }
 
 // verify therapists
-const verifyTherapistController = async (therapistId, verifyStatus) => {
+const verifyTherapistController = async (req, res) => {
     try {
+        const therapistId = req.params.therapistId
+        const { verifyStatus } = req.body;
         const { status } = await manageTherapistQueries.verifyTherapistQuery(therapistId, verifyStatus)
         if (status === 'ok') {
-            return { status }
+            res.status(200).json({
+                status: 'ok',
+                message: 'Therapist verified succesfully'
+            });
         }
     } catch (err) {
+        res.status(401).json({ status: 'nok', message: 'Invalid entry' })
         console.log('Error found', err)
     }
 }
 
 // delet therapist
-const deleteTherapistController = async (therapistId) => {
+const deleteTherapistController = async (req, res) => {
     try {
+        const therapistId = req.params.therapistId
         const { status } = await manageTherapistQueries.deleteTherapistsQuery(therapistId);
         if (status === 'ok') {
-            return { status: 'ok' }
+            res.status(200).json({
+                status: 'ok',
+                message: 'User blocked succesfully'
+            });
         }
     } catch (err) {
+        res.status(401).json({ status: 'nok', message: 'Invalid entry' })
         console.log('Error found', err)
     }
 }
 
 // edit therapist
-const editTherapistController = async (therapistId) => {
+const editTherapistController = async (req, res) => {
     try {
+        const therapistId = req.params.therapistId
         const { status } = await manageTherapistQueries.editTherapistsQuery(therapistId);
         if (status === 'ok') {
-            return { status: 'ok' }
+            res.status(200).json({
+                status: 'ok',
+                message: 'User unblocked succesfully'
+            });
         }
     } catch (err) {
+        res.status(401).json({ status: 'nok', message: 'Invalid entry' })
         console.log('Error found', err)
 
     }
