8972dc6348c15eb49aa4d0764149680dd2726c08
Anusree-mj <anuanusree682@gmail.com>
2024-05-29 23:09:04 +0530

    arrange user routes codes

--- backend/interface/routes/userLogin/userRoutes.js
+++ backend/interface/routes/userLogin/userRoutes.js
@@ -4,82 +4,13 @@ const router = express.Router();
 import controllers from '../../../useCases/index.js';
 
 //loginuser
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    console.log(req.body,'fksdjfksjdfjsdfjsdkl')
-    const response = await controllers.userAuthControllers.authUser(email, password);
-    console.log(response,'responsein routesssssssssss')
-    if (response.status === 'ok') {
-      console.log('reached back in loginroutes')
-      if (response.role === 'client') {
-        res.cookie('jwtClient', response.token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true });
-        console.log(response.client);
-        res.status(200).json({ status: 'ok', role: 'client', client: response.client });
-      }
-      else {
-        res.cookie('jwtTherapist', response.token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true });
-        console.log(response.therapist);
-        res.status(200).json({ status: 'ok', role: 'therapist', therapist: response.therapist });
-      }
-    } else {
-      const { message } = response;
-      console.log(message,'messageeeeeeeeeee')
-      res.status(401).json({ status: 'nok', message });
+router.post('/login', controllers.userAuthControllers.authUser);
 
-    }
-  } catch (error) {
-    console.log(error)
-  }
-});
+router.post('/getOtp', controllers.userAuthControllers.getOtpController)
 
-router.post('/getOtp', async (req, res) => {
-  try {
-    const { email } = req.body;
-    console.log('entered in get otp route')
-    const response = await controllers.userAuthControllers.getOtpController(email)
-    if (response.status === 'ok') {
-      console.log('otp sent')
-      res.status(200).json({ status: 'ok' });
-    } else {
-      res.status(400).json({ status: 'nok', message: 'User already exists' })
-    }
-  } catch (err) {
-    console.log(err)
-  }
-})
 // forgot password
-router.post('/forgotPassword/getOtp', async (req, res) => {
-  try {
-    const { email } = req.body;
-    console.log('entered in forgot password get otp route')
-    const { status } = await controllers.userAuthControllers.getForgotPasswordOTP(email)
-    if (status === 'ok') {
-      console.log('otp sent')
-      res.status(200).json({ status: 'ok' });
-    } else {
-      res.status(400).json({ status: 'nok', message: 'Invalid email' })
-    }
-  } catch (err) {
-    console.log(err)
-  }
-})
+router.post('/forgotPassword/getOtp', controllers.userAuthControllers.getForgotPasswordOTP)
 
-router.post('/forgotPassword/verifyOtp', async (req, res) => {
-  try {
-    const { email, otp } = req.body;
-    console.log('entered in forgot password get otp route', email, otp)
-
-    const { status } = await controllers.userAuthControllers.verifyOTP(email, otp)
-    if (status === 'ok') {
-      console.log('otp matched')
-      res.status(200).json({ status: 'ok' });
-    } else {
-      res.status(400).json({ status: 'nok', message: 'Invalid OTP' })
-    }
-  } catch (err) {
-    console.log(err)
-  }
-})
+router.post('/forgotPassword/verifyOtp', controllers.userAuthControllers.verifyOTP)
 
 export default router;
--- backend/useCases/users/userAuthController.js
+++ backend/useCases/users/userAuthController.js
@@ -4,26 +4,28 @@ import clientAuthQueries from '../../infrastructure/dbQueries/client/clientAuthQ
 import { generateOTP, sendOtpByEmail } from '../../utilitis/generateOTP.js';
 
 // auth user
-const authUser = async (email, password) => {
+const authUser = async (req, res) => {
     try {
-        console.log('entereed in user controllerrrrrrrrr');
+        const { email, password } = req.body;
         const response = await userAuthQueries.userDoLogin(email, password);
         if (response.status === 'ok') {
             console.log('response got from query')
             if (response.role === 'client') {
                 const { status, client } = response;
                 const token = generateToken(client._id);
-                return { status, client, token, role: 'client' }
+                res.cookie('jwtClient', token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true });
+                res.status(200).json({ status: status, role: 'client', client: client });
             }
             else {
                 const { status, therapist } = response;
                 const token = generateToken(therapist._id);
-                return { status, therapist, token, role: 'therapist' }
+                res.cookie('jwtTherapist', token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true });
+                res.status(200).json({ status: status, role: 'therapist', therapist: therapist });
             }
         } else {
             console.log('response nok invalid ')
             const { status, message } = response
-            return { status, message }
+            res.status(401).json({ status: status, message: message });
         }
     } catch (error) {
         console.log('Error found', error)
@@ -31,19 +33,20 @@ const authUser = async (email, password) => {
 }
 
 // getOtp controller
-const getOtpController = async (email) => {
+const getOtpController = async (req, res) => {
     try {
+        const { email } = req.body;
         const verifyUser = await userAuthQueries.checkUser(email);
         if (verifyUser.status === 'ok') {
             const otp = generateOTP()
-            console.log(otp,'otp')
+            console.log(otp, 'otp')
             const response = await clientAuthQueries.saveOtp(email, otp);
             if (response.status === 'ok') {
                 await sendOtpByEmail(email, otp);
-                return { status: 'ok' };
+                res.status(200).json({ status: 'ok' });
+            } else {
+                res.status(400).json({ status: 'nok', message: 'User already exists' })
             }
-        } else {
-            return { status: 'nok', message: 'User already exists' }
         }
     } catch (err) {
         console.log('Error found', err)
@@ -53,32 +56,34 @@ const getOtpController = async (email) => {
 
 
 // forgot password get otp
-const getForgotPasswordOTP = async (email) => {
+const getForgotPasswordOTP = async (req, res) => {
     try {
+        const { email } = req.body;
         const verifyUser = await userAuthQueries.verifyEmail(email);
         if (verifyUser.status === 'ok') {
             const otp = generateOTP()
+            console.log(otp, 'otp')
             const response = await clientAuthQueries.saveOtp(email, otp);
             if (response.status === 'ok') {
                 await sendOtpByEmail(email, otp);
-                return { status: 'ok' };
+                res.status(200).json({ status: 'ok' });
+            } else {
+                res.status(400).json({ status: 'nok', message: 'Invalid email' })
             }
-        } else {
-            return { status: 'nok', message: 'Email not found' }
         }
     } catch (err) {
         console.log('Error found', err)
     }
 }
 
-const verifyOTP = async (email, otp) => {
+const verifyOTP = async (req, res) => {
     try {
-        console.log('entered in verify otp controller')
+        const { email, otp } = req.body;
         const { status } = await userAuthQueries.verifyOTPQuery(email, otp);
         if (status === 'ok') {
-            return { status: 'ok' }
+            res.status(200).json({ status: 'ok' });
         } else {
-            return { status: 'nok', message: 'Invalid OTP' }
+            res.status(400).json({ status: 'nok', message: 'Invalid OTP' })
         }
     } catch (err) {
         console.log('Error found', err)
