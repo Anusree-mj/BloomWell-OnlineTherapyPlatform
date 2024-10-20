1a4bd7ee0dbf66c88faad763627eac85f7a33457
Anusree-mj <anuanusree682@gmail.com>
2024-10-20 11:38:33 +0530

    fix:flickering at client ongoing activity

--- frontend/app/admin/login/page.tsx
+++ frontend/app/admin/login/page.tsx
@@ -7,8 +7,23 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import store from '../../../store'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
+import Loading from "@/components/common/loading";
 
 const Page = () => {
+    const [loading, setLoading] = useState(true)
+    const router = useRouter()
+    useEffect(() => {
+        if (localStorage.getItem('adminData')) {
+            router.push('/admin');
+        } else {
+            setLoading(false)
+        }
+    }, [router]);
+    if (loading) {
+        return (<Loading />)
+    }
     return (
         <Provider store={store}>
             <ToastContainer />
--- frontend/app/client/myActivity/ongoing/page.tsx
+++ frontend/app/client/myActivity/ongoing/page.tsx
@@ -6,15 +6,63 @@ import 'react-toastify/dist/ReactToastify.css';
 import ClientHeader from '@/components/client/header/clientHeader';
 import Footer from '@/components/common/footer';
 import OngoingActivityComponent from '@/components/client/navicons/activities/ongoingAtivityComponent';
+import { useEffect, useState } from 'react';
+import { clientAuth } from '@/utilities/auth';
+import { getClientOngoingActivityAction } from "@/store/clients/clientMyActionReducer";
+import { useDispatch } from 'react-redux';
+import { Box } from '@mui/material';
+
+// Define the type for therapist details
+interface TherapistDetails {
+  _id: string;
+  name: string;
+}
 
 const Page = () => {
+  const [loading, setLoading] = useState(true); // Loading state
+  const [therapistDetails, setTherapistDetails] = useState<TherapistDetails | null>(null); // Allow therapistDetails to be either null or a TherapistDetails object
+  const [ongoingActivity, setOngoingActivity] = useState([]); // Initialize ongoingActivity as an empty array
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = clientAuth();
+      if (response.status === 'ok') {
+        const { clientDetails } = response;
+        if (clientDetails?.isConnected && clientDetails?.therapistDetails) {
+          setTherapistDetails(clientDetails.therapistDetails); // Now it can safely be an object
+
+          // Dispatch the action to fetch ongoing activities
+          const result = await dispatch(getClientOngoingActivityAction({ therapistId: clientDetails.therapistDetails._id }));
+          setOngoingActivity(result.payload); // Assuming the result is available as payload
+        }
+      }
+      setLoading(false); // Stop loading after data fetching is done
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  if (loading) {
     return (
-        <Provider store={store}>
-            <ToastContainer />
-            <ClientHeader />
-            <OngoingActivityComponent />
-            <Footer />
-        </Provider>
+      <Box sx={{
+        display: 'flex', backgroundColor: '#325343',
+        flexDirection: 'column', minHeight: '80vh',
+        alignItems: 'center', justifyContent: 'center', pb: 8
+      }}>
+        <p>Loading...</p> {/* Replace with a proper spinner if needed */}
+      </Box>
     );
-}
-export default Page
+  }
+
+  return (
+    <Provider store={store}>
+      <ToastContainer />
+      <ClientHeader />
+      <OngoingActivityComponent therapistDetails={therapistDetails} ongoingActivity={ongoingActivity} />
+      <Footer />
+    </Provider>
+  );
+};
+
+export default Page;
