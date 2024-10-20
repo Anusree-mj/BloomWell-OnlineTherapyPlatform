8c3f0e50b09db2456dace417a3c8eccbcd8c7a70
Anusree-mj <anuanusree682@gmail.com>
2024-10-20 12:29:39 +0530

    revert:client ongoing page flickering

--- frontend/components/client/navicons/activities/ongoingAtivityComponent.tsx
+++ frontend/components/client/navicons/activities/ongoingAtivityComponent.tsx
@@ -1,71 +1,79 @@
-'use client'
-import { Box } from "@mui/material";
+'use-client'
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getClientOngoingActivityAction, clientMyActivityStateType } from "@/store/clients/clientMyActionReducer";
+import { io } from 'socket.io-client'
+import { useRouter } from "next/navigation";
+import { Box, Button, MenuItem, Select, TextField, Typography } from "@mui/material";
 import { GridColDef } from '@mui/x-data-grid';
 import DoSomethingComponent from "../../../common/doSomethingComponent";
 import TableComponent from "@/components/common/tableComponent";
+import { clientStateType } from "@/store/clients/clientReducer";
+import { clientAuth } from "@/utilities/auth";
 
-// Define the types for therapistDetails and ongoingActivity
-interface TherapistDetails {
-  _id: string;
-  name: string;
-}
 
-interface OngoingActivityItem {
-  _id: string;
-  date: string;
-  sessionStart?: string;
-  sessionEnd?: string;
-  sessionDuration?: string;
-  status: string;
-}
+const OngoingActivityComponent = () => {
+    const dispatch = useDispatch();
+    const therapistDetails = useSelector((state: { client: clientStateType }) => state.client.client.therapistDetails);
+    const ongoingActivity = useSelector((state: { clientMyActivity: clientMyActivityStateType }) => state.clientMyActivity.ongoingActivities);
+    const router = useRouter()
 
-interface OngoingActivityComponentProps {
-  therapistDetails: TherapistDetails | null;
-  ongoingActivity: OngoingActivityItem[];
-  isTable:Boolean
-}
 
-const OngoingActivityComponent: React.FC<OngoingActivityComponentProps> = ({ therapistDetails, ongoingActivity,isTable }) => {
-  const columns: GridColDef[] = [
-    { field: "slNo", headerName: "Sl.No", width: 70 },
-    { field: "date", headerName: "Date", width: 130 },
-    { field: "startedAt", headerName: "Started At", width: 150 },
-    { field: "endedAt", headerName: "Ended At", width: 150 },
-    { field: "duration", headerName: "Duration", width: 130 },
-    { field: "status", headerName: "Status", width: 130 },
-  ];
+    useEffect(() => {
+        const response = clientAuth()
+        if (response.status === 'ok') {
+            const { clientDetails } = response
+            if (clientDetails?.isConnected && clientDetails?.therapistDetails) {
+                dispatch(getClientOngoingActivityAction({ therapistId: clientDetails?.therapistDetails._id }));
+            }
+        }
+    }, []);
 
-  const rows = ongoingActivity.map((item, index) => ({
-    id: item._id,
-    slNo: index + 1,
-    date: item.date,
-    startedAt: item.sessionStart ? item.sessionStart : '---',
-    endedAt: item.sessionEnd ? item.sessionEnd : '---',
-    duration: item.sessionDuration ? item.sessionDuration : '---',
-    status: item.status
-  }));
+    const columns: GridColDef[] = [
+        { field: "slNo", headerName: "Sl.No", width: 70 },
+        { field: "date", headerName: "Date", width: 130 },
+        { field: "startedAt", headerName: "Started At", width: 150 },
+        { field: "endedAt", headerName: "Ended At", width: 150 },
+        { field: "duration", headerName: "Duration", width: 130 },
+        { field: "status", headerName: "Status", width: 130 },
+    ];
 
-  let head;
-  if (therapistDetails) {
-    head = `Therapist : ${therapistDetails.name}`;
-  }
+    const rows = ongoingActivity.map((item, index) => ({
+        id: item._id,
+        slNo: index + 1,
+        date: item.date,
+        startedAt: item.sessionStart ? item.sessionStart : '---',
+        endedAt: item.sessionEnd ? item.sessionEnd : '---',
+        duration: item.sessionDuration ? item.sessionDuration : '---',
+        status: item.status
+    }));
+    let head;
+    if (therapistDetails) {
+        head = `Therapist : ${therapistDetails.name}`;
+    }
+    const subHead = [
+        { name: 'Ongoing', url: 'client/myActivity/ongoing', select: true },
+        { name: 'All', url: 'client/myActivity/all', select: false },
+    ]
+    return (
+        <Box sx={{
+            backgroundColor: '#325343', pb: 8
+        }}>
+            {therapistDetails && therapistDetails.name !== '' ? (
+                <TableComponent rows={rows} columns={columns} head={head ? head : ''} subHead={subHead} role='' />
 
-  const subHead = [
-    { name: 'Ongoing', url: 'client/myActivity/ongoing', select: true },
-    { name: 'All', url: 'client/myActivity/all', select: false },
-  ];
-
-  return (
-    <Box sx={{ backgroundColor: '#325343', pb: 8 }}>
-      {isTable? (
-        <TableComponent rows={rows} columns={columns} head={head || ''} subHead={subHead} role='' />
-      ) : (
-        <Box sx={{ display: 'flex', backgroundColor: '#325343', flexDirection: 'column', minHeight: '80vh', alignItems: 'center', justifyContent: 'center', pb: 8 }}>
-          <DoSomethingComponent content="You haven't connected to any therapist yet!" buttonTitle="Let's Connect" url="/client/connection" />
+            ) : (
+                <Box sx={{
+                    display: 'flex', backgroundColor: '#325343',
+                    flexDirection: 'column', minHeight: '80vh',
+                    alignItems: 'center', justifyContent: 'center', pb: 8
+                }}>
+                    <DoSomethingComponent
+                        content=" You haven't connected to any therapist yet!" buttonTitle="Let's Connect"
+                        url="/client/connection" />
+                </Box>
+            )}
         </Box>
-      )}
-    </Box>
-  );
-};
-
-export default OngoingActivityComponent;
+    )
+}
+export default OngoingActivityComponent
